6f495eceb72bee0ebe15679a20ea19dfbbcd482b
erbaocui <cuijiapeng@gmail.com>
2017-11-06 13:27:44 +0800

    更新常量

---
+++ src/main/java/com/cn/constant/EsIndex.java
@@ -0,0 +1,13 @@
+package com.cn.constant;
+
+import com.cn.util.PropertiesUtil;
+
+/**
+ * Created by home on 2017/11/4.
+ */
+public class EsIndex
+{
+    public static final String INDEX_ACTION_LOG ="actionlog" ;
+    public static final String TYPE_ACTION_LOG ="logtype" ;
+
+}
