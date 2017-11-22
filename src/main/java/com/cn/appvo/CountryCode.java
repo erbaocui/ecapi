2dafcf5162efa6af422b03a465b42e5f53f1f8dd
erbaocui <cuijiapeng@gmail.com>
2017-11-22 09:52:07 +0800

    国家代码
    国家代码

---
+++ src/main/java/com/cn/appvo/CountryCode.java
@@ -0,0 +1,29 @@
+package com.cn.appvo;
+
+/**
+ * Created by cuijp on 2017/11/21.
+ */
+public class CountryCode {
+
+    private String country;
+    private String code;
+
+    public CountryCode() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
